53bc5d2748626700228e3e0f3e9a23d78061fe91
Osama Dahnoun <omdahnou@ncsu.edu>
2021-11-12 17:14:08 -0500

    created new html page(singl-repo)

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,31 +1,38 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
-
-
-
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
-    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-    fetch(apiUrl)
-    .then(function(response) {
-        //request was successful
-        if (response.ok) {
-            response.json().then(function(data) {
-                displayIssues(data);
-            });
+  // format the github api url
+  var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
         }
-        else {
-            alert("There was a problem with your request!")
-        }
-    });
+      });
+    }
+    else {
+      console.log(response);
+      alert("There was a problem with your request!");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
-    if (issues.length === 0) {
-        issueContainerEl.textContent = "This repo has no open issues!";
-        return;
-      }
-     // loop over given issues
+  if (issues.length === 0) {
+    issueContainerEl.textContent = "This repo has no open issues!";
+    return;
+  }
+
+  // loop over given issues
   for (var i = 0; i < issues.length; i++) {
     // create a link element to take users to the issue on github
     var issueEl = document.createElement("a");
@@ -58,12 +65,20 @@ var displayIssues = function(issues) {
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
-
-  issueContainerEl.appendChild(issueEl);
-
-    
 };
 
-  getRepoIssues("osamadahnoun/timed-code-quiz");
+var displayWarning = function(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-  displayIssues();
+  // create link element
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
